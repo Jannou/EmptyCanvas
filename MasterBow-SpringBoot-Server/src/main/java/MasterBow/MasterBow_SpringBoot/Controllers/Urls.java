4a10d3f10db0eeec267a087b4068112ba2937096
Jannou <jannoub@gmail.com>
2020-08-17 20:44:44 +0200

    init commit

---
+++ MasterBow-SpringBoot-Server/src/main/java/MasterBow/MasterBow_SpringBoot/Controllers/Urls.java
@@ -0,0 +1,8 @@
+package MasterBow.MasterBow_SpringBoot.Controllers;
+
+public class Urls {
+
+    public static final String API = "/api";
+    public static final String PING = "/ping";
+    public static final String DB = "/db";
+}
---
+++ MasterBow-SpringBoot-Server/src/test/java/MasterBow/MasterBow_SpringBoot/MasterBowSpringBootApplicationTests.java
@@ -0,0 +1,13 @@
+package MasterBow.MasterBow_SpringBoot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MasterBowSpringBootApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
